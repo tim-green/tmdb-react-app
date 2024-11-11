0c340fed6493c7224c7bca0e9f380cb290f61513
Tim <github@timgreen.ws>
2024-11-11 16:54:28 +1100

    updated 404 page

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -8,7 +8,7 @@ const PageNotFound = () => {
   return (
     <div className="pageNotFound">
       <ContentWrapper>
-        <span className="bigText">404</span>
+        <span className="bigText">4<span className="error0">0</span>4</span>
         <span className="smallText">Page not found!</span>
       </ContentWrapper>
     </div>
