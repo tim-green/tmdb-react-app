7220ee43cce5385bf800f5e38c70628915a3a6ed
Tim <github@timgreen.ws>
2024-11-11 13:37:16 +1100

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import preload from "vite-plugin-preload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),preload()],
+  server: {
+    port: 3006,
+  },
 })
+
